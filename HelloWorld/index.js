4eb4d62013e2c93eb9c5baa31099a96f88cf914e
akira_cn <akira.cn@gmail.com>
2013-12-19 12:21:38 +0800

    update

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -1,3 +1,8 @@
 /** default boot file **/
 
+/**
+ 	NOTE: while you update (add or remove) commands
+ 	you may restart sublime text or resave the v8.py file 
+ 	in plugin SublimeJS to make changes effect
+ */
 defineCommand("Hello", require("hello.command"));
